e9957d4423a11d3104d8d3fe143587609ad59534
Orsen <orsen.kucher@gmail.com>
2020-07-06 12:16:06 +0300

    sync ch4

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,14 @@
 fn main() {
     let str_literal = "hello";
-    let s = String::from(str_literal);
+    let mut s = String::from(str_literal);
+
+    s.push_str(", world");
 
     println!("{}", s);
+
+    let x = String::from(str_literal);
+    let y = x.clone();
+
+    println!("{}", x);
+    println!("{}", y);
 }
