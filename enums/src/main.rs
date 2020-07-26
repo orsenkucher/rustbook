bbefb6144e291ee7797726f343747e0402428332
orsenkucher <orsen.kucher@gmail.com>
2020-07-26 22:27:35 +0300

    ch6: enums

---
+++ enums/src/main.rs
@@ -0,0 +1,42 @@
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    let none: Option<i32> = None;
+    println!("{:#?}", none);
+
+    let coin = Coin::Quarter(UsState::Alaska);
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+    println!("Count {:?}!", count);
+
+    let coin = Coin::Quarter(UsState::Alabama);
+    value_in_cents(coin);
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
