46faa5da77a54a937f0aed555bfd0ebfa502cc09
orsenkucher <orsen.kucher@gmail.com>
2020-08-11 01:25:54 +0300

    ch12: main

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,15 @@
+use std::{env, fs};
+
+// $ cargo run test sample.txt
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Serching for {}", query);
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("With text:\n{}", contents);
 }
