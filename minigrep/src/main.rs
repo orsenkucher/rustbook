d7b4acbe5d58fb2915cb888855099d74ed6ec8a9
orsenkucher <orsen.kucher@gmail.com>
2020-08-11 01:58:33 +0300

    ch12: fix: error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,10 +1,13 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 // $ cargo run test sample.txt
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Serching for {}", config.query);
     println!("In file {}", config.filename);
@@ -20,10 +23,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
