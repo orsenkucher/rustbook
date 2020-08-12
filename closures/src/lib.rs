71556c0313a8ec5d82e0a4c6fe7f0925301b53ce
orsenkucher <orsen.kucher@gmail.com>
2020-08-12 11:38:11 +0300

    ch13: closures

---
+++ closures/src/lib.rs
@@ -0,0 +1,30 @@
+use std::thread;
+use std::time::Duration;
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+}
+
+pub fn generate_workout(intensity: u32, random_nuber: u32) {
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            simulated_expensive_calculation(intensity)
+        );
+        println!(
+            "Today, do {} situps!",
+            simulated_expensive_calculation(intensity)
+        );
+    } else {
+        if random_nuber == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                simulated_expensive_calculation(intensity)
+            );
+        }
+    }
+}
---
+++ closures/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    closures::generate_workout(simulated_user_specified_value, simulated_random_number);
+}
