eacffd3ac4e2dfed03dbf1f2c9f9668306c555e5
orsenkucher <orsen.kucher@gmail.com>
2020-08-12 12:13:44 +0300

    ch13: closure in cacher

--- closures/src/lib.rs
+++ closures/src/lib.rs
@@ -1,30 +1,55 @@
 use std::thread;
 use std::time::Duration;
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
-
 pub fn generate_workout(intensity: u32, random_nuber: u32) {
+    let mut expensive_closure = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
     if intensity < 25 {
-        println!(
-            "Today, do {} pushups!",
-            simulated_expensive_calculation(intensity)
-        );
-        println!(
-            "Today, do {} situps!",
-            simulated_expensive_calculation(intensity)
-        );
+        println!("Today, do {} pushups!", expensive_closure.value(intensity));
+        println!("Today, do {} situps!", expensive_closure.value(intensity));
     } else {
         if random_nuber == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
             println!(
                 "Today, run for {} minutes!",
-                simulated_expensive_calculation(intensity)
+                expensive_closure.value(intensity)
             );
+        }
+    }
+}
+
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Self {
+        Self {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
         }
     }
 }
