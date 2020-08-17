881f31930d78d04583588b7f5d1da5ac4b6bae8e
Arseniy Kucher <orsen.kucher@gmail.com>
2020-08-18 01:21:59 +0300

    wow, thanks @Plecra

--- smart_pointers/src/lib.rs
+++ smart_pointers/src/lib.rs
@@ -1,31 +1,22 @@
-// todo: https://rust-unofficial.github.io/too-many-lists/
-
-enum List<T> {
+pub enum List<T> {
     Cons(T, Box<List<T>>),
     Nil,
 }
 
-struct ListIter<'a, T>(&'a Box<List<T>>);
+pub struct Iter<'a, T>(&'a List<T>);
 
-impl<'a, T> std::ops::Deref for ListIter<'a, T> {
-    type Target = &'a Box<List<T>>;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl<'a, T> IntoIterator for &'a Box<List<T>> {
+impl<'a, T> IntoIterator for &'a List<T> {
     type Item = &'a T;
-    type IntoIter = ListIter<'a, T>;
+    type IntoIter = Iter<'a, T>;
     fn into_iter(self) -> Self::IntoIter {
-        ListIter(self)
+        Iter(self)
     }
 }
 
-impl<'a, T> Iterator for ListIter<'a, T> {
+impl<'a, T> Iterator for Iter<'a, T> {
     type Item = &'a T;
     fn next(&mut self) -> Option<Self::Item> {
-        if let List::Cons(v, ll) = self as &List<_> {
+        if let List::Cons(v, ll) = &*self.0 {
             self.0 = ll;
             Some(v)
         } else {
@@ -40,7 +31,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let list = Box::new(Cons(2, Box::new(Cons(1, Box::new(Nil)))));
+        let list = Cons(2, Box::new(Cons(1, Box::new(Nil))));
         assert_eq!(list.into_iter().collect::<Vec<_>>(), vec![&2, &1])
     }
 }
