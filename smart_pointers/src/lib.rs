7e7ab51effa278c2a4b71a1b080f55e274fd5979
Arseniy Kucher <orsen.kucher@gmail.com>
2020-08-17 14:16:19 +0300

    ch15: wip: cons list

--- smart_pointers/src/lib.rs
+++ smart_pointers/src/lib.rs
@@ -1,25 +1,63 @@
-// enum List<T> {
-//     Cons(T, Box<List<T>>),
-//     Nil,
-// }
-
-enum List {
-    Cons(i32, Box<List>),
+enum List<T> {
+    Cons(T, Box<List<T>>),
     Nil,
 }
 
-use crate::List::{Cons, Nil};
+struct ListIter<'a, T> {
+    val: &'a Box<List<T>>,
+}
 
-impl Iterator for List {
-    type Item = i32;
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Cons(val, list) = self {
-            Some(*val)
-        } else {
-            None
+impl<'a, T> IntoIterator for List<T> {
+    type Item = T;
+    type IntoIter = ListIter<'a, T>;
+    fn into_iter(self) -> Self::IntoIter {
+        ListIter {
+            val: Box::new(&self),
         }
     }
 }
+
+// https://rust-unofficial.github.io/too-many-lists/
+impl<T> Iterator for ListIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        // let b = self.val;
+        // self.val.
+
+        // let val = &*self.val;
+        if let List::Cons(val, list) = &*self.val {
+            self.val = list;
+            return Some(val);
+        }
+
+        // let v: List<Self::Item> = self.val.into();
+        None
+        // if let List::Cons(val, list) = self.val {
+        //     self.val = *list;
+        //     Some(val)
+        // } else {
+        //     None
+        // }
+    }
+}
+
+// enum List {
+//     Cons(i32, Box<List>),
+//     Nil,
+// }
+
+// use crate::List::{Cons, Nil};
+
+// impl Iterator for List {
+//     type Item = i32;
+//     fn next(&mut self) -> Option<Self::Item> {
+//         if let Cons(val, list) = self {
+//             Some(*val)
+//         } else {
+//             None
+//         }
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
