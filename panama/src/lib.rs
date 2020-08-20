51a3e014f6ef525157ffdbf810a5b680d35b918f
Arseniy Kucher <orsen.kucher@gmail.com>
2020-08-21 00:18:38 +0300

    ref: panama

--- panama/src/lib.rs
+++ panama/src/lib.rs
@@ -1,7 +1,8 @@
 use std::collections::VecDeque;
 use std::sync::{Arc, Condvar, Mutex};
+
 pub struct Sender<T> {
-    inner: Arc<Inner<T>>,
+    shared: Arc<Shared<T>>,
 }
 
 // need to impl Clone ourselves, since Arc is clonable not necessarily when T is clonable
@@ -9,8 +10,8 @@ impl<T> Clone for Sender<T> {
     fn clone(&self) -> Self {
         Sender {
             // specifically clone the Arc and not the thing inside the Arc:
-            inner: Arc::clone(&self.inner),
-            // when self.inner.clone() might clone inner Arc value as well.
+            shared: Arc::clone(&self.shared),
+            // when self.shared.clone() might clone shared Arc value as well.
             // x. is (*x). and it recurses down
         }
     }
@@ -18,51 +19,51 @@ impl<T> Clone for Sender<T> {
 
 impl<T> Sender<T> {
     pub fn send(&mut self, t: T) {
-        let mut queue = self.inner.queue.lock().unwrap();
+        let mut queue = self.shared.queue.lock().unwrap();
         queue.push_back(t);
         drop(queue); // drop lock to make receiver wakeup
-        self.inner.available.notify_one();
+        self.shared.available.notify_one();
     }
 }
 
 pub struct Receiver<T> {
-    inner: Arc<Inner<T>>,
+    shared: Arc<Shared<T>>,
 }
 
 impl<T> Receiver<T> {
     pub fn recv(&mut self) -> T {
-        let mut queue = self.inner.queue.lock().unwrap();
+        let mut queue = self.shared.queue.lock().unwrap();
         loop {
             match queue.pop_front() {
                 Some(t) => return t,
                 None => {
                     // wait until OS gives a reason to wake, though it's not guaranteed
                     // the reason is what we wait for. So we loop here
-                    queue = self.inner.available.wait(queue).unwrap();
+                    queue = self.shared.available.wait(queue).unwrap();
                 }
             }
         }
     }
 }
 
-struct Inner<T> {
+struct Shared<T> {
     queue: Mutex<VecDeque<T>>,
     available: Condvar,
 }
 
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
-    let inner = Inner {
+    let shared = Shared {
         queue: Mutex::default(),
         available: Condvar::new(),
     };
-    let inner = Arc::new(inner);
+    let shared = Arc::new(shared);
     (
         Sender {
-            // Arc::clone(&inner) is more idiomatic? see section about clone above
-            inner: inner.clone(),
+            // Arc::clone(&shared) is more idiomatic? see section about clone above
+            shared: shared.clone(),
         },
         Receiver {
-            inner: inner.clone(),
+            shared: shared.clone(),
         },
     )
 }
