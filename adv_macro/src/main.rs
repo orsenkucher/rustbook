832c4662f57ed9f9fdd1538f5cbd1517212080a2
Arseniy Kucher <orsen.kucher@gmail.com>
2020-09-13 19:32:46 +0300

    ch19: macro_rules!

--- adv_macro/src/main.rs
+++ adv_macro/src/main.rs
@@ -6,4 +6,17 @@ struct Pancakes;
 
 fn main() {
     Pancakes::hello_macro();
+
+    println!("{:?}", my_vec![1, 2, 3]);
+}
+
+#[macro_export]
+macro_rules! my_vec {
+    ($($x:expr),*) => {{
+        let mut temp_vec = Vec::new();
+        $(
+            temp_vec.push($x);
+        )*
+        temp_vec
+    }};
 }
