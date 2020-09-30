54cba8570d0195e1901578ce96ae744c7f9b145f
Arseniy Kucher <orsen.kucher@gmail.com>
2020-10-01 00:59:32 +0300

    using local wasm pkg

--- wa-life/src/lib.rs
+++ wa-life/src/lib.rs
@@ -14,6 +14,6 @@ extern {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wa-life!");
+pub fn greet(name: &str) {
+    alert(&format!("Hello {} from wa-life!", name));
 }
--- wa-life/www/index.js
+++ wa-life/www/index.js
@@ -1,3 +1,3 @@
-import * as wasm from "hello-wasm-pack";
+import * as wasm from "wa-life";
 
-wasm.greet();
+wasm.greet("Orsen");
