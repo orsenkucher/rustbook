565f66c4d1db25d6cfd4ede2578235080383dc5c
Arseniy Kucher <orsen.kucher@gmail.com>
2020-10-04 00:01:49 +0300

    ch20: selective response

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -7,7 +7,6 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
         handle_connection(stream);
     }
 }
@@ -16,13 +15,19 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n", "404.html")
+    };
 
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let contents = fs::read_to_string(filename).unwrap();
     let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        "{}Content-Length: {}\r\n\r\n{}",
+        status_line,
         contents.len(),
-        contents,
+        contents
     );
 
     stream.write(response.as_bytes()).unwrap();
