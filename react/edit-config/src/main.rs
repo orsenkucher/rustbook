9ea907deec13d01399827151bafef8f375505fa6
Arseniy Kucher <orsen.kucher@gmail.com>
2020-10-11 00:36:05 +0300

    config: reapplying decor

--- react/edit-config/src/main.rs
+++ react/edit-config/src/main.rs
@@ -22,7 +22,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     // decor.()
     // toml_edit::Decor::
 
-    doc["hello"] = value("toml2");
+    // doc["hello"] = value("toml2");
+    let val_raw = doc["hello"].as_value_mut().unwrap();
+    // value
+
+    let val_decor = val_raw.decor();
+    *val_raw = toml_edit::decorated(
+        toml_edit::Value::from("toml2!"),
+        val_decor.prefix(),
+        val_decor.suffix(),
+    );
+    doc["hello"].as_inline_table_mut().map(|t| t.fmt());
+    // val_raw.as_inline_table_mut().map(|e| e.fmt());
     // val.as_str().unwrap() = "Heh";
     // autoformat inline table a.b.c: { d = "hello" }
     doc["a"]["b"]["c"].as_inline_table_mut().map(|t| t.fmt());
