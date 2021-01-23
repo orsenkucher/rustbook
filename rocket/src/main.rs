5b1d22bba48fd396e1ecdfb24544ac1096322f0a
Arseniy Kucher <orsen.kucher@gmail.com>
2021-01-23 22:31:59 +0200

    try rocket

---
+++ rocket/src/main.rs
@@ -0,0 +1,20 @@
+#[macro_use]
+extern crate rocket;
+
+#[cfg(test)]
+mod tests;
+
+#[get("/hello/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[get("/hello/<name>")]
+fn hi(name: String) -> String {
+    name
+}
+
+#[launch]
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![hello, hi])
+}
---
+++ rocket/src/tests.rs
@@ -0,0 +1,36 @@
+use rocket::http::Status;
+use rocket::local::blocking::Client;
+
+fn test(uri: String, expected: String) {
+    let client = Client::tracked(super::rocket()).unwrap();
+    assert_eq!(client.get(&uri).dispatch().into_string(), Some(expected));
+}
+
+fn test_404(uri: &'static str) {
+    let client = Client::tracked(super::rocket()).unwrap();
+    assert_eq!(client.get(uri).dispatch().status(), Status::NotFound);
+}
+
+#[test]
+fn test_hello() {
+    for &(name, age) in &[("Mike", 22), ("Michael", 80), ("A", 0), ("a", 127)] {
+        test(
+            format!("/hello/{}/{}", name, age),
+            format!("Hello, {} year old named {}!", age, name),
+        );
+    }
+}
+
+#[test]
+fn test_failing_hello() {
+    test_404("/hello/Mike/1000");
+    test_404("/hello/Mike/-129");
+    test_404("/hello/Mike/-1");
+}
+
+#[test]
+fn test_hi() {
+    for name in &["Mike", "A", "123", "hi", "c"] {
+        test(format!("/hello/{}", name), name.to_string());
+    }
+}
