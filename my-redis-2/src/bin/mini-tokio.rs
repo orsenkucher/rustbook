22192426c5a6765674ca9b7da56b2bd415d0d4ef
Arseniy <orsen.kucher@gmail.com>
2022-01-09 04:32:22 +0200

    fix: fix notify clone

--- my-redis-2/src/bin/mini-tokio.rs
+++ my-redis-2/src/bin/mini-tokio.rs
@@ -90,7 +90,7 @@ async fn delay(dur: Duration) {
 
     let when = Instant::now() + dur;
     let notify = Arc::new(Notify::new());
-    let notify2 = Arc::new(Notify::new());
+    let notify2 = notify.clone();
 
     thread::spawn(move || {
         let now = Instant::now();
