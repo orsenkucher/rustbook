fcdfc241a2cfb2917b906f240f3163d4190bbd19
Arseniy <orsen.kucher@gmail.com>
2022-01-09 04:20:13 +0200

    fix: Delay future

--- my-redis-2/src/bin/mini-tokio.rs
+++ my-redis-2/src/bin/mini-tokio.rs
@@ -1,30 +1,50 @@
 use crossbeam::channel;
-use futures::lock::Mutex;
 use futures::task::{self, ArcWake};
 use std::future::Future;
 use std::pin::Pin;
 use std::sync::Arc;
-use std::task::{Context, Poll};
+use std::sync::Mutex;
+use std::task::{Context, Poll, Waker};
 use std::thread;
 use std::time::{Duration, Instant};
 
 struct Delay {
     when: Instant,
+    // This Some when we have spawned a thread, and None otherwise.
+    waker: Option<Arc<Mutex<Waker>>>,
+}
+
+impl Delay {
+    fn new(when: Instant) -> Self {
+        Delay { when, waker: None }
+    }
 }
 
 impl Future for Delay {
     type Output = &'static str;
 
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        if Instant::now() >= self.when {
-            println!("Delay done");
-            Poll::Ready("done")
-        } else {
-            // Get a handle to the waker for the current task
-            let waker = cx.waker().clone();
-            let when = self.when;
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        // First, if this is the first time the future is called, spawn the
+        // timer thread. If the timer thread is already running, ensure the
+        // stored `Waker` matches the current task's waker.
+        if let Some(waker) = &self.waker {
+            let mut waker = waker.lock().unwrap();
 
-            // Spawn a timer thread.
+            // Check if the stored waker matches the current task's waker.
+            // This is necessary as the `Delay` future instance may move to
+            // a different task between calls to `poll`. If this happens, the
+            // waker contained by the given `Context` will differ and we
+            // must update our stored waker to reflect this change.
+            if !waker.will_wake(cx.waker()) {
+                *waker = cx.waker().clone();
+            }
+        } else {
+            // Spawn new thread
+            let when = self.when;
+            let waker = Arc::new(Mutex::new(cx.waker().clone()));
+            self.waker = Some(waker.clone());
+
+            // This is the first time `poll` is called, spawn the timer thread.
             thread::spawn(move || {
                 let now = Instant::now();
 
@@ -32,8 +52,34 @@ impl Future for Delay {
                     thread::sleep(when - now);
                 }
 
-                waker.wake();
+                // The duration has elapsed. Notify the caller by invoking
+                // the waker.
+                let waker = waker.lock().unwrap();
+                waker.wake_by_ref();
             });
+        }
+
+        // Once the waker is stored and the timer thread is started, it is
+        // time to check if the delay has completed. This is done by
+        // checking the current instant. If the duration has elapsed, then
+        // the future has completed and `Poll::Ready` is returned.
+        if Instant::now() >= self.when {
+            println!("Delay done");
+            Poll::Ready("done")
+        } else {
+            // The duration has not elapsed, the future has not completed so
+            // return `Poll::Pending`.
+            //
+            // The `Future` trait contract requires that when `Pending` is
+            // returned, the future ensures that the given waker is signalled
+            // once the future should be polled again. In our case, by
+            // returning `Pending` here, we are promising that we will
+            // invoke the given waker included in the `Context` argument
+            // once the requested duration has elapsed. We ensure this by
+            // spawning the timer thread above.
+            //
+            // If we forget to invoke the waker, the task will hang
+            // indefinitely.
             Poll::Pending
         }
     }
@@ -44,8 +90,7 @@ fn main() {
 
     mini_tokio.spawn(async {
         let when = Instant::now() + Duration::from_millis(10);
-        let future = Delay { when };
-
+        let future = Delay::new(when);
         let out = future.await;
         assert_eq!(out, "done");
     });
